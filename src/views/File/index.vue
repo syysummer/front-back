a8f2e36827a3ea032797256964118ab1acff0419
sunyangyang <sunyangyang@creality.com>
2022-02-14 18:25:01 +0800

    [add] File demo

---
+++ src/views/File/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="file-page">
+    {{$t('File.file')}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Mixins, Vue } from 'vue-property-decorator'
+import { mapState } from 'vuex'
+
+@Component({
+  computed: {
+    ...mapState([
+      'config'
+    ])
+  }
+})
+export default class File extends Mixins(Vue,) {
+}
+</script>
+
+<style lang="less" scoped>
+  .file-page {
+    .main-container();
+    padding: 1.25rem;
+  }
+</style>
